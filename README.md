dc32316c63d5a55955622d8c8c04a38b28856eda
learn-modern-technology <mailmeatss2112@gmail.com>
2023-09-28 06:54:44 +0530

    Successfully Tested Model Training Engine

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # basic-text-processing-and-classification-model
- To understand the basic text preprocessing and build a classification model
+This repository contains the code for basic text processing and building a binary text classifier using Logistic Regression
+
+### Installation
+To install the dependencies run:
+```buildoutcfg
+pip install -r requirements.txt
+```
+
+### Dataset
+The dataset is a custom dataset where reviews about an app are taken from app store and the reviews are classified either as positive or negative
+
+### Train the model
+To train the model run:
+```buildoutcfg
+python Engine.py --file_name Canva_reviews.xlsx --vectorizer binary_bag_of_words --output_name binary_count_vector
+```
+Here we can use 4 types of vectorizers:
+* Bag of Words - `nonbinary_bag_of_words`
+* Binary Bag of Words - `binary_bag_of_words`
+* N-grams - `ngram`
+* TF-IDF - `term_frequency_inverse_document_frequency`
+
+### Predictions
+To make prediction on a new review `Its the worst app ever I save my design lts not save`,  run:
+```buildoutcfg
+python predict.py --text 'Its the worst app ever I save my design lts not save' --model_name binary_count_vector
+```
+Here `binary_count_vect` is the file name used to save the model and the vectorizer during the training phase
+
+### Note on NLTK Package:
+For installing NLTK, use the command `pip install nltk` <br />
+After downloading, the NLTK corpus has to be downloaded <br />
+Run `import nltk` followed by `nltk.download()` in jupyter notebook <br />
+This will open a separate window where you can donwnload the necessary packages <br />
+For this project, you will need the following packages:<br />
+<ol>
+<li>punkt</li>
+<li>stopwords</li>
+<li>wordnet</li>
+</ol>
---
+++ src/model.py
@@ -0,0 +1,58 @@
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+import config
+from src.utils import save_file
+
+
+def custom_vectorizer(i_token_list, y, vector_output_name, i_vector='binary_bag_of_words', i_min_df=5, i_ng_low=1, i_ng_high=3, i_test_size=0.2, i_rs=42):
+    """
+    Given list of tokens and the dependent variables, the function will
+    vectorize the tokens, split the set into train and test and returns the
+    data along with the vectorizer
+    :param i_token_list: List of processed tokens
+    :param y: Dependent variable
+    :param i_vector: Vectorizer ('binary_bag_of_words' for count vectors, 'nonbinary_bag_of_words' for binary count vectors, 'ngram' for n-grams and 'term_frequency_inverse_document_frequency' for tf-idf
+    :param i_min_df: min_df parameter in CountVectorizer
+    :param i_ng_low: Lower value for n-gram
+    :param i_ng_high: Higher value for n-gram
+    :param i_test_size: Size of test split
+    :param i_rs: random seed
+    :return: train and test vectors (both X and y), vectorizer
+    """
+    try:
+
+        # Create vectorizer file path
+        vectorized_file = os.path.join(config.output_path, f"{vector_output_name}.pkl")
+        # Create vectorizer
+        if i_vector == 'nonbinary_bag_of_words':
+            p_vectorizer = CountVectorizer(min_df=i_min_df)
+        elif i_vector == 'binary_bag_of_words':
+            p_vectorizer = CountVectorizer(binary=True, min_df=i_min_df)
+        elif i_vector == 'ngram':
+            p_vectorizer = CountVectorizer(min_df=i_min_df, ngram_range=(i_ng_low, i_ng_high))
+        elif i_vector == 'term_frequency_inverse_document_frequency':
+            p_vectorizer = TfidfVectorizer(min_df=i_min_df)
+        else:
+            raise Exception("Vector has to be one of 'nonbinary_bag_of_words', 'binary_bag_of_words', 'ngram', "
+                            "'term_frequency_inverse_document_frequency'")
+
+        # Fit the vectorizer
+        # p_vectorizer.fit(i_token_list)
+
+        # Fit the vectorizer and tranform the data and assign to X
+        X = p_vectorizer.fit_transform(i_token_list)
+
+        # Save the vectorizer
+        save_file(vectorized_file, p_vectorizer)
+
+        # Split the data into train and test
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=i_test_size,
+                                                            stratify=y, random_state=i_rs)
+
+    except Exception as e:
+        print('An excpetion in custom_vectorizer()-', e)
+    else:
+        return X_train, X_test, y_train, y_test, p_vectorizer
